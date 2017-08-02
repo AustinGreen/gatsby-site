c7c48966a209f0e315b87050555e81e28ab51dbf
Austin Green <austinzgreen@gmail.com>
2017-08-02 16:12:37 -0400

    have to commit public

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import bg from '../img/bg.jpg';
 
 export default () =>
   <div>
+    <img src={bg} alt="bg" />
     <p>Hello world from my home page</p>
     <Link to="/about/">About</Link>
   </div>;
